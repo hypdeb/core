3991df0dd9b3907d97993c2ac017fc2ca44f58e6
hypdeb <julien.debache@hotmail.com>
2021-12-12 16:51:56 +0100

    style

--- src/InCube.Core/Collections/AsyncEnumerables.cs
+++ src/InCube.Core/Collections/AsyncEnumerables.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Threading.Tasks;
 
 namespace InCube.Core.Collections;
 
--- src/InCube.Core/CoreExtensions.cs
+++ src/InCube.Core/CoreExtensions.cs
@@ -4,5 +4,4 @@ namespace InCube.Core;
 
 public class CoreExtensions
 {
-    
 }
